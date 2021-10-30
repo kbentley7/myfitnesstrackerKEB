c655f3a9a6a1110eab27c082313a07151d99e5fe
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-30 16:48:34 -0400

    edited api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -52,11 +52,14 @@ router.get("/api/workouts/range", (req, res) => {
   });
 
   //Route to add exercise to current workout
-  router.post("/api/workouts", ({ body }, res) => {
-    console.log(body);
-    db.Workout.create({})
-      .then((newWorkout) => {
-        res.json(newWorkout);
+  router.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      req.params.id,
+      { $push: { exercises: req.body } },
+      { new: true }
+    )
+      .then((updateWorkout) => {
+        res.json(updateWorkout);
       })
       .catch((err) => {
         res.status(400).json(err);
