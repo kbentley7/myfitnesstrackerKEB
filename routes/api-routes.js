9003abb6d88e3d5fb01f0dcf4ec1a21f16a54c5b
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-30 16:38:02 -0400

    edited api-routes.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,10 +2,17 @@ const router = require("express").Router();
 const db = require("../models");
 
 router.get("/api/workouts", (req, res) => {
-    db.workout.aggregate([
-        {
-          $addFields: {
-            totalDuration: { $sum: "$exercises.duration" },
-          },
+    db.Workout.aggregate([
+      {
+        $addFields: {
+          totalDuration: { $sum: "$exercises.duration" },
         },
-})
+      },
+    ])
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
